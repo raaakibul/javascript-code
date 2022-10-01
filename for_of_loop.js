248026b03b914d62a74c3b7fbec83013a4a8b061
rakibul-islam <raakibul.uap@gmail.com>
2022-10-01 23:27:15 +0600

    for each loop code file added

---
+++ for_of_loop.js
@@ -0,0 +1,13 @@
+var nameArray = ["Rakibul", "Rafi", "Karim", "Rahim"]
+
+console.log("Name :");
+for(let name of nameArray){
+    console.log(name);
+}
+
+var country = ["Pakistan", "Bangladesh","India", "Sri-lanka"]
+
+console.log("Country name:");
+for(let name of country){
+    console.log(name);
+}
